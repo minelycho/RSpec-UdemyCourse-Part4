18aaa00f00ed1a61d1b84b8ee01f8baa8cfaa438
Minerva <romanagrimm7@gmail.com>
2021-05-28 00:16:35 -0600

    Equality Matchers II (equal and be)

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -15,8 +15,25 @@ RSpec.describe 'equality matchers' do
 			expect(a).not_to eql(3)
 			expect(b).not_to eql(3.0)
 			expect(a).not_to eql(b)
+		end
+	end
 
-			
+	describe 'equal and be matcher' do 
+		let(:c) { [1, 2, 3] }
+		let(:d) { [1, 2, 3] }
+		let(:e) { c }
+
+		it 'cares about object identity' do
+			#eq values
+			expect(c).to eq(d)
+			#eql values and types
+			expect(c).to eql(d)
+			#be an alias of equal
+			expect(c).to equal(e)
+			expect(c).to be(e)
+
+			expect(c).not_to be(d)
+			expect(c).not_to be([1, 2, 3])
 		end
 	end
 end
