bccbb1eedc1c25adfbd40300d7a1ce7460ad633a
Minerva <romanagrimm7@gmail.com>
2021-06-02 02:38:44 -0600

    Replacing an Object with a Double

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -30,14 +30,32 @@ class Movie
 
  	def start_shooting
  		if actor.ready?
- 			puts actor.act
- 			puts actor.fall_off_ladder
- 			puts actor.light_on_fire
+ 			actor.act
+ 			actor.fall_off_ladder
+ 			actor.light_on_fire
  		end
  	end
 end
 
-
+=begin
 actor = Actor.new('Brad Pitt')
 movie = Movie.new(actor)
 movie.start_shooting 
+
+=end
+
+RSpec.describe Movie do 
+	let(:stunman) { double("Mr. Danger", ready?: true, act: 'Any string at all',
+		fall_off_ladder: "agent phone", light_on_fire: false)}
+		subject { described_class.new(stunman)}
+
+	describe '#start_shooting method' do 
+		it 'expects an actor to do 3 actions' do 
+			expect(stunman).to receive(:ready?)
+			expect(stunman).to receive(:act)
+			expect(stunman).to receive(:fall_off_ladder)
+			expect(stunman).to receive(:light_on_fire)
+			subject.start_shooting
+		end
+	end
+end
