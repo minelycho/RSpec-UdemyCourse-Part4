0147a945c6ff1fd9480b15f91d227722f538ca19
Minerva <romanagrimm7@gmail.com>
2021-06-03 02:11:23 -0600

    Receive Counts

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -33,6 +33,7 @@ class Movie
  			actor.act
  			actor.fall_off_ladder
  			actor.light_on_fire
+ 			actor.act
  		end
  	end
 end
@@ -45,16 +46,19 @@ movie.start_shooting
 =end
 
 RSpec.describe Movie do 
-	let(:stunman) { double("Mr. Danger", ready?: true, act: 'Any string at all',
+	let(:stuntman) { double("Mr. Danger", ready?: true, act: 'Any string at all',
 		fall_off_ladder: "agent phone", light_on_fire: false)}
-		subject { described_class.new(stunman)}
+		subject { described_class.new(stuntman)}
 
 	describe '#start_shooting method' do 
 		it 'expects an actor to do 3 actions' do 
-			expect(stunman).to receive(:ready?)
-			expect(stunman).to receive(:act)
-			expect(stunman).to receive(:fall_off_ladder)
-			expect(stunman).to receive(:light_on_fire)
+			#expect(stuntman).to receive(:light_on_fire).once
+			#expect(stuntman).to receive(:light_on_fire).exactly(1).times
+			expect(stuntman).to receive(:light_on_fire).at_most(1).times
+
+			#expect(stuntman).to receive(:act).twice
+			#expect(stuntman).to receive(:act).exactly(2).times
+			expect(stuntman).to receive(:act).at_least(2).times
 			subject.start_shooting
 		end
 	end
